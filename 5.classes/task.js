08ea2998fcfc62d4f5c5b977fb5e5c1fe1625231
Vera <kuprivm@mail.ru>
2025-10-27 18:13:57 +0300

    Решение пятого задания 3

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,86 +1,81 @@
 class PrintEditionItem {
-	constructor(name, releaseDate, pagesCount) {
+    constructor(name, releaseDate, pagesCount, state = 100, type = null) {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
-        this.state = 100;
-        this.type = null;
+        this._state = state;
+        this.type = type;
     }
-	fix() {
-		this._state *= 1.5;
-	}
-	set state(value) {
-		if (value < 0) {
-			this._state = 0;
-		} else if (value > 100) {
-			this._state = 100;
-		} else {
-			this._state = value;
-		}
-	}
-	get state() {
-		return this._state;
-	}
+    set state(newState) {
+        if (newState < 0) {
+            this._state = 0;
+        } else if (newState > 100) {
+            this._state = 100;
+        } else {
+            this._state = newState;
+        }
+    }
+    get state() {
+        return this._state;
+    }
+    fix() {
+        this.state *= 1.5; 
+        }
 }
 
 class Magazine extends PrintEditionItem {
-	constructor(name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount);
-		type = "magazine";
-	}
+    constructor(name, releaseDate, pagesCount, state = 100, type = "magazine") {
+        super(name, releaseDate, pagesCount, state, type);
+    }
 }
+
 class Book extends PrintEditionItem {
-	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount);
-		this.author = author;
-		type = "book";
-	}
+    constructor(author, name, releaseDate, pagesCount, state = 100, type = "book") {
+        super(name, releaseDate, pagesCount, state, type);
+        this.author = author;
+    }
 }
+
 class NovelBook extends Book {
-	constructor(author, name, releaseDate, pagesCount) {
-		super(author, name, releaseDate, pagesCount);
-		type = "novel";
+    constructor(author, name, releaseDate, pagesCount, state = 100, type = "novel") {
+        super(author, name, releaseDate, pagesCount, state, type);
 	}
 }
-class FantasticBook extends Book {
-	constructor(author, name, releaseDate, pagesCount) {
-		super(author, name, releaseDate, pagesCount);
-		type = "fantastic";
-	}
+
+class FantasticBook extends Book{
+   constructor(author, name, releaseDate, pagesCount, state = 100, type = "fantastic") {
+    super(author, name, releaseDate, pagesCount, state, type);
+   } 
 }
+
 class DetectiveBook extends Book {
-	constructor(author, name, releaseDate, pagesCount) {
-		super(author, name, releaseDate, pagesCount);
-		type = "detective";
-	}
-}
+    constructor(author, name, releaseDate, pagesCount, state = 100, type = "detective") {
+     super(author, name, releaseDate, pagesCount, state, type);
+    }
+ }
 //Задача 2
 
-class Library {
-	constructor(name) {
-		this.name = name;
-		this.books = [];
-	}
-
-	addBook(book) {
-		if (book.state > 30) {
-			this.books.push(book);
-		}
-	}
-
-	findBookBy(type, value) {
-        const foundBook = this.books.find(book => book[type] === value);
-        return foundBook !== undefined ? foundBook : null;
+class Library{
+    constructor(name, books = []) {
+        this.name = name;
+        this.books = [...books];
     }
-
-	giveBookByName(bookName) {
-        const index = this.books.findIndex(book => book.name === bookName);
-        if (index !== -1) {
-            const book = this.books[index];
-            this.books.splice(index, 1);
-            return book;
-        } else {
-            return null;
+    addBook(book) {
+        if (book?.state > 30) {
+            this.books.push(book);
         }
     }
+    findBookBy(type, value) {
+        const result = this.books.find((book) =>
+             book.hasOwnProperty(type) && book[type] === value ) ; 
+        return result || null;
+    }
+    giveBookByName(bookName) {
+        const index = this.books.findIndex(book => book?.name === bookName);
+        if (index !== -1) {
+            return this.books.splice(index, 1)[0];
+        }
+        return null;
+    }
+
 }
