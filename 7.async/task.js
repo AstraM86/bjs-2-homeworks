d5f2421dbbec560538df0dd3f4f191175c260362
Vera <kuprivm@mail.ru>
2025-11-10 17:43:33 +0300

    Решение седьмого задания.Исправление1

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,42 +3,60 @@ class AlarmClock {
     this.alarmCollection = [];
     this.intervalId = null;
   }
+
   addClock(time, callback) {
     if (!time || !callback) {
       throw new Error('Отсутствуют обязательные аргументы');
-    } else if ( this.alarmCollection.find(setup => setup.time === time) ) {
+    }
+
+    if (this.alarmCollection.some(alarm => alarm.time === time)) {
       console.warn('Уже присутствует звонок на это же время');
     }
-    this.alarmCollection.push({time, callback, canCall: true});
+
+    this.alarmCollection.push({
+      time,
+      callback,
+      canCall: true,
+    });
   }
+
   removeClock(time) {
-    this.alarmCollection = this.alarmCollection.filter(
-      setup => setup.time !== time
-    )
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
   }
+
   getCurrentFormattedTime() {
-    return new Date().toLocaleTimeString().slice(0, -3);
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
   }
+
   start() {
-    if (this.intervalId) {
-      return
+    if (this.intervalId !== null) {
+      return;
     }
-    this.intervalId = setInterval( () => {
-      this.alarmCollection.forEach( setup => {
-        if (setup.time === this.getCurrentFormattedTime() && setup.canCall) {
-          setup.canCall = false;
-          setup.callback();          
+
+    this.intervalId = setInterval(() => {
+      const currentTime = this.getCurrentFormattedTime();
+
+      this.alarmCollection.forEach(alarm => {
+        if (alarm.time === currentTime && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
         }
-      })
+      });
     }, 1000);
   }
+
   stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
   }
+
   resetAllCalls() {
-    this.alarmCollection.forEach( setup => setup.canCall = true);
+    this.alarmCollection.forEach(alarm => alarm.canCall = true);
   }
+
   clearAlarms() {
     this.stop();
     this.alarmCollection = [];
